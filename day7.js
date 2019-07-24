9bcccce535f1d37948437f37e889dc53a36421fa
ANJALI CHOURASIA <38558953+anjalichourasia@users.noreply.github.com>
2019-07-24 10:49:52 +0530

    Update day7.js
    1- ^ asserts position at start of the string 
    2- 1st Capturing Group ([aeiou]) 
    3- .* matches any character (except newline) - between zero and unlimited times, as many times as possible, giving back as needed (greedy) 
    4- \1 matches the same text as most recently matched by the 1st capturing group 
    5- $ asserts position at the end of the string

--- day7.js
+++ day7.js
@@ -48,3 +48,10 @@ function main() {
     
     console.log(re.test(s));
 }
+/*
+1- ^ asserts position at start of the string 
+2- 1st Capturing Group ([aeiou]) 
+3- .* matches any character (except newline) - between zero and unlimited times, as many times as possible, giving back as needed (greedy) 
+4- \1 matches the same text as most recently matched by the 1st capturing group 
+5- $ asserts position at the end of the string
+*/
