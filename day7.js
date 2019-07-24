fa37b23544d3a5dd2bbff7b58d7052df85515b7e
ANJALI CHOURASIA <38558953+anjalichourasia@users.noreply.github.com>
2019-07-24 10:45:54 +0530

    hackerank
    day 7

---
+++ day7.js
@@ -0,0 +1,50 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
+     */
+    
+    // let re = /\b[aeiou]\b/;
+    // The.* means that there may be characters after that first captured character 0 or more
+    //In this situation, .* will allow for something like aa, but .+ will not.
+
+// .+ makes it so that there has to be at least one character between the first and the last, and thus enforces the constraint that the string length must be >= 3.
+    // \1$ checks that the last character matches the first captured charcter
+    let re = /^([aeiou]).+\1$/;
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+
+
+function main() {
+    const re = regexVar();
+    const s = readLine();
+    
+    console.log(re.test(s));
+}
